85ce8ead748af378dd20bfb659d26f7b30372da0
Woonani <nada3377@naver.com>
2023-06-29 15:33:32 +0900

    add: workspace service

--- otree/src/main/java/com/otree/douzone/controller/WorkspaceRestController.java
+++ otree/src/main/java/com/otree/douzone/controller/WorkspaceRestController.java
@@ -3,6 +3,8 @@ package com.otree.douzone.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +24,22 @@ public class WorkspaceRestController {
 		this.workspaceService = workspaceService;
 	}
 	
+	// 워크스페이스 생성
 	@PostMapping
 	public ResponseEntity<String> createWorkspace(@RequestBody Workspace workspace) {
-		System.out.println("workspace : "+workspace);
+		System.out.println("workspace : " + workspace);
 		workspaceService.createWorkspace(workspace);
 		System.out.println("insert성공");
 		return ResponseEntity.status(HttpStatus.CREATED).body("insert success");
+	}
+	
+	// 유저의 전체 워크스페이스 목록 조회 
+	@GetMapping("/{userId}")
+	public ResponseEntity<String> getWorkspaceList (@PathVariable("userId") int userId) {
+		System.out.println("userId : " + userId);
+		workspaceService.getWorkspaceList(userId);
+		System.out.println("select성공");
+		return ResponseEntity.status(HttpStatus.CREATED).body("select success");
 	}
 	
 }
